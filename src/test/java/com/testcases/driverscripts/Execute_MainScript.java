075421e671554674b639a924a3c232ebebf345fc
RKLogixal <rohit.karkhanis@logixal.com>
2020-03-30 10:45:36 +0530

    Catch block for ElementClickInterceptedException

--- src/test/java/com/testcases/driverscripts/Execute_MainScript.java
+++ src/test/java/com/testcases/driverscripts/Execute_MainScript.java
@@ -15,6 +15,7 @@ import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.TimeoutException;
@@ -299,10 +300,10 @@ public class Execute_MainScript {
 
 			} catch (TimeoutException Te) {
 
-
+				
 				Te.printStackTrace(new PrintWriter(stack));
 				Applog.error(stack.toString());
-				FC.FailedTCOperation(Testcase_description, screxe, webdriver, xls_writer, Testscase_failresults, browser_name, Functionality, Testcasenumber, Severity, StartTime, Startdate, softAssert, test, extent);
+				FC.FailedTCOperation(webdriver,Testcase_description, screxe, webdriver, xls_writer, Testscase_failresults, browser_name, Functionality, Testcasenumber, Severity, StartTime, Startdate, softAssert, test, extent);
 
 			}
 
@@ -310,7 +311,14 @@ public class Execute_MainScript {
 
 				Nse.printStackTrace(new PrintWriter(stack));
 				Applog.error(stack.toString());
-				FC.FailedTCOperation(Testcase_description, screxe, webdriver, xls_writer, Testscase_failresults, browser_name, Functionality, Testcasenumber, Severity, StartTime, Startdate, softAssert, test, extent);
+				FC.FailedTCOperation(webdriver,Testcase_description, screxe, webdriver, xls_writer, Testscase_failresults, browser_name, Functionality, Testcasenumber, Severity, StartTime, Startdate, softAssert, test, extent);
+
+			}
+			catch(ElementClickInterceptedException Ce) {
+
+				Ce.printStackTrace(new PrintWriter(stack));
+				Applog.error(stack.toString());
+				FC.FailedTCOperation(webdriver,Testcase_description, screxe, webdriver, xls_writer, Testscase_failresults, browser_name, Functionality, Testcasenumber, Severity, StartTime, Startdate, softAssert, test, extent);
 
 			}
 			catch(Exception e) {
